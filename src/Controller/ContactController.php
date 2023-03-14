14ba9308f22d875b3973247aea600d4b21006312
Mael <mael.buisson2@gmail.com>
2023-03-14 13:12:56 +0100

    :sparkles: Adding an Email sending function

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,48 +5,62 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-
-  
-
+use Symfony\Component\Mailer\MailerInterface;
 
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
     public function index(
-        EntityManagerInterface $manager,
-        Request $request
-        ): Response
-    {
+        Request $request,
+        MailerInterface $mailer,
+        EntityManagerInterface $manager
+    ): Response {
 
 
         $contact = new Contact();
 
         if ($this->getUser()) {
             $contact->setFullName($this->getUser()->getFullName())
-                    ->setEmail($this->getUser()->getEmail());
+                ->setEmail($this->getUser()->getEmail());
         }
 
         $form = $this->createForm(ContactType::class, $contact);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-              $contact = $form->getData();
+            $contact = $form->getData();
 
-              $manager->persist($contact);
-              $manager->flush();
+            $manager->persist($contact);
+            $manager->flush();
 
-              $this->addFlash(
+            //Email
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('admin@comicsstorage.com')
+                ->subject($contact->getSubject())
+                ->htmlTemplate('emails/contact.html.twig')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'contact' => $contact
+                ]);
+
+            $mailer->send($email);
+
+
+
+            $this->addFlash(
                 'success',
                 'Votre demande a bien été prise en compte'
-             );
+            );
 
-              return $this->redirectToRoute('contact.index');
-
+            return $this->redirectToRoute('contact.index');
         }
 
         return $this->render('pages/contact/index.html.twig', [
